6de97beed05c0c15cdd97d39d85c996d62487984
bakuzan <imannoyed4days@hotmail.co.uk>
2019-12-09 09:39:34 +0000

    More dep mess.

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import '@babel/register';
 import dotenv from 'dotenv';
 
-import replace from 'rollup-plugin-replace';
+import replace from '@rollup/plugin-replace';
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
