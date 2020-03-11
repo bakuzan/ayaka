f41eb799fb89711d77ee247d6485dce0f5c9cf80
bakuzan <imannoyed4days@hotmail.co.uk>
2020-03-11 09:48:38 +0000

    Improved localStorage for server rendering.

--- lib/localStorage.ts
+++ lib/localStorage.ts
@@ -1,3 +1,6 @@
+/* tslint:disable:no-empty */
+import { prop } from './helpers/prop';
+
 export default class Store<T> {
   private storeName: string;
   private defaultValue: T;
@@ -7,34 +10,43 @@ export default class Store<T> {
     this.defaultValue = defaultValue;
   }
 
+  private get storage() {
+    return typeof localStorage !== undefined
+      ? localStorage
+      : {
+          getItem: (key: string) => '',
+          setItem: (key: string, value: string) => {}
+        };
+  }
+
   public get(): T {
-    const item = localStorage.getItem(this.storeName) || '';
+    const item = this.storage.getItem(this.storeName) || '';
     const data = (item ? JSON.parse(item) : this.defaultValue) as T;
 
     return { ...this.defaultValue, ...data };
   }
 
-  public getKey(key: keyof T): T[keyof T] {
-    const item = localStorage.getItem(this.storeName) || '';
+  public getKey<K extends keyof T>(key: K) {
+    const item = this.storage.getItem(this.storeName) || '';
     const data = (item ? JSON.parse(item) : this.defaultValue) as T;
 
-    return data[key] || this.defaultValue[key];
+    return prop(data, key) ?? prop(this.defaultValue, key);
   }
 
   public set(mergeValues: Partial<T>): T {
     const values = this.get();
     const updated: T = { ...values, ...mergeValues };
-    localStorage.setItem(this.storeName, JSON.stringify(updated));
+    this.storage.setItem(this.storeName, JSON.stringify(updated));
     return updated;
   }
 
   public replace(newValue: T): T {
     const data = JSON.stringify(newValue);
-    localStorage.setItem(this.storeName, data);
+    this.storage.setItem(this.storeName, data);
     return this.get() as T;
   }
 
-  public upgrade(...upgradeFns: Array<(data: Partial<T>) => Partial<T>>) {
+  public upgrade(...upgradeFns: ((data: Partial<T>) => Partial<T>)[]) {
     const data = this.get();
     const upgradedData = upgradeFns.reduce((upD, fn) => fn(upD), data);
     this.replace(upgradedData as T);
